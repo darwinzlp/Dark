cc2640077b7dc50a991ef5b037d376fda9c8ca16
darwinzlp <139069054+darwinzlp@users.noreply.github.com>
2023-07-09 12:33:48 -0500

    Daarle

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Ancelsiñho\nBot\nMD', {
+say('Daarle\nBot\nMD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Creado por Ancelbot`, {
+say(`Creado por Darlee`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
